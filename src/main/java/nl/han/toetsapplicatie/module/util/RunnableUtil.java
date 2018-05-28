72e0f7c539d8be2004817eb9c21a8c432bc892f0
Ihsan Cingisiz <I.Cingisiz@student.han.nl>
2018-05-28 14:36:06 +0200

    Feeback comment verwerkt

--- src/main/java/nl/han/toetsapplicatie/module/util/RunnableUtil.java
+++ src/main/java/nl/han/toetsapplicatie/module/util/RunnableUtil.java
@@ -7,13 +7,11 @@ public class RunnableUtil {
     public static void runIfNotNull(Runnable runnable) {
         if (runnable != null)
             runnable.run();
-        ;
     }
 
     public static <T> void runIfNotNull(Consumer<T> runnable, T val) {
         if (runnable != null)
             runnable.accept(val);
-        ;
     }
 
 }
