57bbe19c5a349533a6076d9269cf94c26892fee3
Ihsan Cingisiz <I.Cingisiz@student.han.nl>
2018-05-31 11:58:46 +0200

    Removed gitkeep and added dummy test for buildfix

---
+++ src/test/java/nl/han/toetsapplicatie/module/model/ToetsTest.java
@@ -0,0 +1,13 @@
+package nl.han.toetsapplicatie.module.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ToetsTest {
+
+    @Test
+    public void getNaam() {
+        assertEquals(true, true);
+    }
+}
