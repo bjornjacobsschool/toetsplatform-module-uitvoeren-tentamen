bb03152a60b5f78c20963ab5df1bfa3513a1bfa0
Sefa Senturk <RS.Senturk@student.han.nl>
2018-06-04 09:54:27 +0200

    Tentamen is now being saved when download is pressed

--- src/main/java/nl/han/toetsplatform/module/uitvoeren_tentamen/dao/downloaden_tentamen/DownloadenTentamenDAO.java
+++ src/main/java/nl/han/toetsplatform/module/uitvoeren_tentamen/dao/downloaden_tentamen/DownloadenTentamenDAO.java
@@ -2,27 +2,38 @@ package nl.han.toetsplatform.module.uitvoeren_tentamen.dao.downloaden_tentamen;
 
 import nl.han.toetsplatform.module.uitvoeren_tentamen.dao.JSONReader;
 import nl.han.toetsplatform.module.uitvoeren_tentamen.model.storage.Tentamen;
-import nl.han.toetsplatform.module.uitvoeren_tentamen.model.storage.Versie;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class DownloadenTentamenDAO implements IDownloadenTentamenDAO {
+
     @Override
-    public boolean downloadTentamen(int tentamenId) {
-        return false;
+    public boolean downloadTentamen(String tentamenId) throws IOException, JSONException {
+
+        // TODO: Replace with real URL
+        JSONObject jTentamen = JSONReader.JSONObjectFromURL("https://www.focusws.nl/exam1.json");
+
+        Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("exam1.json"), StandardCharsets.UTF_8));
+        System.out.println(jTentamen.toString());
+        writer.write(jTentamen.toString());
+        writer.close();
+
+        return true;
     }
 
     @Override
-    public List<Tentamen> getKlaargezetteTentamens() throws IOException, ParseException {
+    public List<Tentamen> getKlaargezetteTentamens() throws IOException, ParseException, JSONException {
         List<Tentamen> tentamens = new ArrayList<>();
 
+        // TODO: Replace with real URL
         JSONArray jTentamens = JSONReader.JSONArrayFromURL("https://www.focusws.nl/exam.json");
 
         for (int i = 0; i < jTentamens.length(); i++) {
