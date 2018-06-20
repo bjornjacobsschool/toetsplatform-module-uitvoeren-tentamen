a5bb61f0e58e5d75f850967fc218307be6873fe2
Ihsan Cingisiz <I.Cingisiz@student.han.nl>
2018-06-20 15:10:31 +0200

    Tijdelijk oplossing door ipv .getData() te doen .getMogelijkAntwoorden() te doen. Dit is afgestemd met de groep 4.

--- src/main/java/nl/han/toetsplatform/module/uitvoeren_tentamen/controllers/TentamenUitvoerenController.java
+++ src/main/java/nl/han/toetsplatform/module/uitvoeren_tentamen/controllers/TentamenUitvoerenController.java
@@ -130,15 +130,15 @@ public class TentamenUitvoerenController extends Controller {
     }
 
     public void loadQuestionView() {
-        questionPane.getChildren().add(currentPlugin.getVraagView(currentTentamen.getVragen().get(currentQuestionIndex).getData()).getView());
+        questionPane.getChildren().add(currentPlugin.getVraagView(currentTentamen.getVragen().get(currentQuestionIndex).getMogelijkeAntwoorden()).getView());
     }
 
     public void loadAnswerView() {
         String localAnswer = loadLocalAnswer();
         if (localAnswer != null) {
-            answerPane.getChildren().add(currentPlugin.getAntwoordView(currentTentamen.getVragen().get(currentQuestionIndex).getData(), localAnswer).getView());
+            answerPane.getChildren().add(currentPlugin.getAntwoordView(currentTentamen.getVragen().get(currentQuestionIndex).getMogelijkeAntwoorden(), localAnswer).getView());
         } else {
-            answerPane.getChildren().add(currentPlugin.getAntwoordView(currentTentamen.getVragen().get(currentQuestionIndex).getData()).getView());
+            answerPane.getChildren().add(currentPlugin.getAntwoordView(currentTentamen.getVragen().get(currentQuestionIndex).getMogelijkeAntwoorden()).getView());
         }
     }
 
